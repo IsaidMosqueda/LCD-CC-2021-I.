82b2575b4e80eea33e1cef3581e5276913a4754f
Pamela-ruiz9 <66136034+Pamela-ruiz9@users.noreply.github.com>
2020-10-12 19:12:54 -0500

    Add files via upload

---
+++ fork2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int i = 1;
+
+  switch(fork()) {
+
+  /* código de error */
+  case -1 :
+    perror ("Error al crear el proceso");
+    exit(-1);
+    break;
+
+  /* código para el hijo */
+  case 0 :
+    while (i<=10){
+      sleep (1);
+      printf("\tSoy el proceso hijo: %d\n", i++);
+    }
+    break;
+
+  /* código para el padre */
+  default:
+    while (i<=10){
+      printf("Soy el proceso padre: %d\n", i++);
+      sleep (2);
+    }
+
+  }
+    return(0);
+}
+
