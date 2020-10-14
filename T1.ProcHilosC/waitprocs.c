4cc8432eef841657c72052179c4ade04e0122442
IsaidMosqueda <71615610+IsaidMosqueda@users.noreply.github.com>
2020-10-14 17:37:59 -0500

    Update waitprocs.c
    Comentarios añadidos

--- T1.ProcHilosC/waitprocs.c
+++ T1.ProcHilosC/waitprocs.c
@@ -6,7 +6,7 @@
 # define NUM_PROCESOS 5
 int I = 0;
 
-void codigo_del_proceso (int id)
+void codigo_del_proceso (int id)     /*Proceso que imprime la información del proceso llamado */
 {
    int i;
 
@@ -19,24 +19,24 @@ void codigo_del_proceso (int id)
 int main(void)
 {
     int p;
-    int id[NUM_PROCESOS] = {1, 2, 3, 4, 5};
+    int id[NUM_PROCESOS] = {1, 2, 3, 4, 5}; /*Crea una lista de ids para los procesos*/
     int pid;
     int salida;
 
-    for (p = 0; p < NUM_PROCESOS; p++) {
+    for (p = 0; p < NUM_PROCESOS; p++) {  /* codigo proceso hijo */
       pid = fork();
-      if  (pid == -1){
+      if  (pid == -1){                              /* Cuando el proceso hijo es -1 se detiene el proceso*/
           perror("Error al crear un proceso: ");
           exit(-1);
       }
       else if (pid == 0) /* codigo proceso hijo */
-          codigo_del_proceso (id[p]);
+          codigo_del_proceso (id[p]);            /* El proceso hijo es imprimir su identificador*/
     }
 
     // codigo proceso padre
     for (p = 0; p < NUM_PROCESOS; p++) {
-        pid = wait(&salida);
-        printf("Proceso %d con id = %x terminado\n", pid, salida >> 8);
+        pid = wait(&salida);     /* Proceso padre espera a que termine el proceso hijo*/
+        printf("Proceso %d con id = %x terminado\n", pid, salida >> 8);   /* Proceso padre imrpime la finalización del proceso hijo*/
     }
 
     return(0);
