06d979430f8b86f097b01d4c085de8e7eaeac2e0
Pamela-ruiz9 <66136034+Pamela-ruiz9@users.noreply.github.com>
2020-10-14 18:24:05 -0500

    Add files via upload

---
+++ T1.ProcHilosC/hilo.c
@@ -0,0 +1,21 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+void *codigo_del_hilo(void *id)
+{
+ int i;          
+ for(i = 0; i < 50; i++) /* Itera el hilo 50  veces */
+  printf("\n Soy el hilo: %d, iter = %d", *(int*)id, i); 
+ pthread_exit(id);
+}
+
+int main(void)
+{
+  pthread_t hilo; /* Inicializa el hilo */
+  int id = 245; /* Numero ID del hilo  */
+  int *salida;
+  pthread_create(&hilo, NULL, codigo_del_hilo, &id); /* Crea el hilo con la la iteración de codigo_del_hilo   */
+  pthread_join(hilo, NULL); /* En espera a la funcion del hilo  */
+  printf("\n Hilo %d terminado \n", *salida); /* Termino dle hilo  */
+}
